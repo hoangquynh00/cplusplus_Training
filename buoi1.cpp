e7dbbf0096a714742c7d4ec1bda0a9aecfe78e4f
hoangquynh00 <hoangquynhhello@gmail.com>
2024-07-06 14:09:44 +0700

    update

---
+++ buoi1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void ChucNguNgon(){
+    cout<<"Con meo de thuong ngu ngon";
+}
+int main()
+{
+    int SoNguyen;
+    float SoThuc;
+    double SoThuc2;
+    char KiTu;
+    int A[100];
+    return 0;
+}
